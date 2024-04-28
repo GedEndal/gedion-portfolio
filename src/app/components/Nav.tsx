e605a9af985c43d5dc550a7e01191b61292df625
GedEndal <n0940732485@Gmail.com>
2024-04-28 15:34:02 +0300

    commit

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import '/nav.css'
+'use client'
+import React,{useState,useEffect} from 'react'
+import './nav.css'
+import { navs } from "../data/data";
 
 export default function Nav() {
+    const [navList ,setNavList] = useState(navs);
   return (
-    <div>Nav</div>
-  )
+    <nav id='navBar' className='navbar nav-menu'>  
+      <ul>
+        {
+          navList.map(nav=>( 
+            <li key={nav.id}>
+            <a className={`nav-link scrollto ${nav.active? 'active' : undefined}`}>
+              <i className={nav.icon}><span>{nav.name}</span>
+                </i></a></li>))}
+      </ul>
+    </nav>
+    )
 }
